6927536cdbd75aa64a7b9817c2bbc6df7ab7bc4b
Priyanka Jain <37510432+priyankapjain@users.noreply.github.com>
2018-03-19 22:53:13 +0530

    logic implementation as per document.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { getTokenService } from "./app.service";
 import { PageEvent } from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  thanksNoteToShow: boolean = false;
+  answer: string='';
+  nextBtnText: string = 'Save & Next';
+  currentQuestion: any;
   title = 'Case Study';
   case_study_questions: any = [];
-  length;
-  pageSize = 1;
-  pageSizeOptions = [5, 10, 25, 100];
-  pageEvent: any = {};
-  paginationPresent:boolean = true;
-  thank_you_note:any='Thank you for attempting Jombay Case Study. Your responses have been recorded successfully';
-  constructor(private getTokenService: getTokenService) {
-    this.pageEvent['pageIndex'] = 0;
+  paginationPresent: boolean = true;
+  currentIndex: number = 0
+  saveData:any=[];
+  thank_you_note: any = 'Thank you for attempting Jombay Case Study. Your responses have been recorded successfully';
+  constructor(private getTokenService: getTokenService,public dialog: MatDialog) {
+   
   }
 
   ngOnInit() {
@@ -31,14 +35,68 @@ export class AppComponent implements OnInit {
     if (this.getTokenService.beare_token) {
       this.getTokenService.getDataFromService().subscribe(res => {
         this.case_study_questions = res.user_company_case_study.company_case_study.questions;
-        this.case_study_questions.push({
-          body:this.thank_you_note
-        });
-        this.length = this.case_study_questions.length;
+        this.currentIndex = 0;
+        this.currentQuestion = this.case_study_questions[this.currentIndex]['body'];
       });
     }
   }
-  getPageNumber(event:any){
-    return this.pageEvent === event;
+
+  getQuestion(item, index) {
+    this.currentQuestion = item['body'];
+    this.currentIndex = index;
   }
+  previousSlide() {
+    this.currentIndex = this.currentIndex - 1;
+    this.currentQuestion = this.case_study_questions[this.currentIndex]['body'];
+    if(this.saveData[this.currentIndex] && this.saveData[this.currentIndex]['answer']){
+      this.answer = this.saveData[this.currentIndex]['answer']?this.saveData[this.currentIndex]['answer']:'';
+    }
+  }
+  nextSlide(answer?:any) {
+    this.thanksNoteToShow = false;
+    if (this.nextBtnText === 'Submit & Exit') {
+        let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+          width: '250px',
+          data: { option:true }
+        });
+    
+        dialogRef.afterClosed().subscribe(result => {
+          console.log('The dialog was closed',result);
+          console.log('final data ',this.saveData);
+          if(result){
+            this.thanksNoteToShow = true;
+          }
+        });
+      
+    } else {
+      this.answer = ''
+      if (this.currentIndex === this.case_study_questions.length - 2) {
+        this.nextBtnText = 'Submit & Exit';
+      } else {
+        this.saveData[this.currentIndex]={
+          question:this.case_study_questions[this.currentIndex]['body'],
+          answer:answer
+        }
+      }
+      this.currentIndex = this.currentIndex + 1;
+      this.currentQuestion = this.case_study_questions[this.currentIndex]['body'];
+    }
+  }
+}
+
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, DialogOverviewExampleDialog } from './app.component';
 import {getTokenService} from "./app.service";
 import {CommonModule} from "@angular/common";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpModule} from "@angular/http";
-import {NgxPaginationModule} from "ngx-pagination";
-import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+import {MatInputModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent , DialogOverviewExampleDialog
   ],
+  entryComponents: [DialogOverviewExampleDialog],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
-    NgxPaginationModule,
-    MatPaginatorModule
+    
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule
   ],
   providers: [getTokenService],
   bootstrap: [AppComponent]
